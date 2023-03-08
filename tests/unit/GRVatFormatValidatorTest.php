57aa532fe34603c2be2a5313e78fc3220024d53d
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-08 20:55:10 +0400

    [CVFV-1] setup test.

--- tests/unit/GRVatFormatValidatorTest.php
+++ tests/unit/GRVatFormatValidatorTest.php
@@ -1,22 +1,21 @@
 <?php
 
-// TODO: set namespace
-namespace unit;
+namespace rocketfellows\GRVatFormatValidator\tests\unit;
 
-class CountryVatFormatValidatorTest extends TestCase
+use PHPUnit\Framework\TestCase;
+
+class GRVatFormatValidatorTest extends TestCase
 {
     /**
-     * TODO: set type
-     * @var
+     * @var GRVatFormatValidator
      */
     private $validator;
 
-    /**
-     * TODO: setup validator for test
-     */
     protected function setUp(): void
     {
-        $this->validator = new CountryVatFormatValidator();
+        parent::setUp();
+
+        $this->validator = new GRVatFormatValidator();
     }
 
     /**
